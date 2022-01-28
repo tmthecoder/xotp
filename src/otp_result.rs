ef759feca050534eee71bece98f50fe433707d6e
Tejas Mehta <tmthecoder@gmail.com>
2022-01-28 11:44:46 -0500

    add in a base OTPResult class with initializers and as_string/as_u32 methods

---
+++ src/otp_result.rs
@@ -0,0 +1,26 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+struct OTPResult {
+    digits: u32,
+    code: u32,
+}
+
+impl OTPResult {
+    pub fn new(digits: u32, code: u32 ) -> Self {
+        OTPResult { digits, code }
+    }
+}
+
+impl OTPResult {
+    pub fn get_digits(&self) -> u32 { self.digits }
+}
+
+impl OTPResult {
+    pub fn as_string(&self) -> String {
+        format!("{:01$}", self.code as usize, self.digits as usize)
+    }
+
+    pub fn as_u32(&self) -> u32 {
+        self.code
+    }
+}
